9bf6648f5aaab60c9d601fe464c24407355b9198
varsha <varshasinghh99@gmail.com>
2025-03-22 14:49:15 +0530

    Adding UC5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  userName: string = "";
+  userName: string = ""; 
+  errorMessage: string = ""; 
+
+  validateUserName(): void {
+    const regex = /^[A-Z][a-zA-Z]{2,}$/; 
+    if (!regex.test(this.userName)) {
+      this.errorMessage = "Invalid name! First letter should be capital & min 3 characters.";
+    } else {
+      this.errorMessage = ""; 
+    }
+  }
 }
 
