83a4ea6596ab0048bcb63b96fd3020c7b7c2caf4
varsha <varshasinghh99@gmail.com>
2025-03-22 14:07:54 +0530

    Adding UC1

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  message: string = "Hello from BridgeLabz! 🚀"; 
+
+}
